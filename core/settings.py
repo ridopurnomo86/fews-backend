4daa8947eb399165be7c94f66c3fed52ff6557e9
Ridho Purnomo <72849578+ridopurnomo86@users.noreply.github.com>
2023-04-08 16:13:06 +0700

    fix allow host and static file

--- core/settings.py
+++ core/settings.py
@@ -31,12 +31,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =  os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = "django-insecure-2f54p-t+1io69sm9gz8%xs)a6fmzr0mbc%^7a^=_xoyh!w%s%l"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = NODE_ENV == 'development'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 INSTALLED_APPS = [
@@ -85,6 +85,10 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'core.wsgi.application'
+
+# Static
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
 # Database
